b6afac09d0b55210b5124773e9a126bd356d7d05
Nischal Acharya <Nischal060@gmail.com>
2024-05-12 14:30:28 +0545

    fixes in user dashbaord

--- resources/views/user_dashboard.blade.php
+++ resources/views/user_dashboard.blade.php
@@ -1,1 +1,17 @@
-hyy this is user dashboard
+@extends('layouts.user')
+
+@section('content')
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">User Dashboard</div>
+
+                <div class="card-body">
+                    <p>Welcome to your dashboard, {{ Auth::user()->name }}!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,10 @@ Route::get('/', function () {
 //admin dashboard
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'role:admin'])->name('dashboard');
 
 //admin profile
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
